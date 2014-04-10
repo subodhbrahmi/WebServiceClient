d76416508adfee1d45ac8f71c58ee2a5086f7d1f
test <gignereindian@hotmail.com>
2014-04-10 10:03:04 -0500

    Initial CheckIn

---
+++ WebServiceClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceClient.ServiceReference1;
+
+namespace WebServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new DelayedStockQuoteSoapClient();
+            var resp = service.GetQuickQuote("NBG","0");
+
+            Console.WriteLine(resp.ToString());
+        }
+    }
+}
